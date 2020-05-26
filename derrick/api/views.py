04319ad90e1bd8c4a10ac97486a9ec5b76aea7d2
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-05-27 02:10:27 +0530

    Added djangorestframework. Basic /posts api endpoint working with auth

--- derrick/api/views.py
+++ derrick/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.serializers import ArticleSerializer
+from api.models import Article
 
-# Create your views here.
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- derrick/derrick/settings.py
+++ derrick/derrick/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_crontab',
     'background_task',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
