8780f0bb1c423660a5d8f3f12981e0dc5d3bc0b0
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-05-26 22:04:20 +0530

    Added code to scrape in scrape_utils. Tested : Working Correctly locally

--- derrick/api/scraper_utils.py
+++ derrick/api/scraper_utils.py
@@ -2,10 +2,13 @@ import time
 import requests
 from bs4 import BeautifulSoup as bs4
 
+from api.models import Article
 
-def scraper_datacenter():
-	print("Parsing has begun")
-	start_time = time.time()
+def get_article_body_from_url(article_url):
+	return("This is the body of the article")
+
+def extract_links_from_homepage():
+	
 	base_url = "https://www.datacenterknowledge.com"
 
 	r = requests.get(base_url)
@@ -23,11 +26,58 @@ def scraper_datacenter():
 			item = {'title' : title, 'url' : url}
 			data.append(item)
 		except:
-			print("Some exception occured")
+			print("Some exception occurred")
 
-	end_time = time.time()
-	print("Time taken : {} seconds".format(end_time - start_time))
 	return data
+
+
+def add_record_to_db(title, url, body):
+	new_article = Article(title=title, url=url, body=body)
+	new_article.save()
+
+
+def article_url_exists_in_db(url):
+
+	exists = False
+
+	try:
+		exists = Article.objects.get(url=url)
+	except:
+		print("URL {} does not exists in DB".format(url))
+
+	return exists
+
+
+
+def scraper_datacenter():
+	print("Parsing has begun")
+
+	items = extract_links_from_homepage()
+
+	if len(items) == 0:
+		print("No links extracted from homepage.")
+		return
+
+	# iterate through all links and check if URL exists in DB
+	# If it doesn't, add it to the DB
+
+	for item in items:
+		article_title = item['title']
+		article_url = item['url']
+
+		if not article_url_exists_in_db(article_url):
+			# scrape and parse the article body from URL
+			article_body = get_article_body_from_url(article_url)
+
+			# Create Model and Save it to DB
+			add_record_to_db(url=article_url, title=article_title, body=article_body)
+
+
+
+
+	
+
+	
 
 
 	
