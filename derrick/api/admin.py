a4394b5d11750d8fe8c19f7fcb55c6ae6f3c0000
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-05-28 05:15:29 +0000

    Added Article model to admin. API working with Auth token

--- derrick/api/admin.py
+++ derrick/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from api.models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Article, ArticleAdmin)
