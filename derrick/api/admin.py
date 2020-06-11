7928862f76db2670d7a3162577453085533278c2
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-06-11 20:53:57 +0000

    Cleanup

--- derrick/api/admin.py
+++ derrick/api/admin.py
@@ -9,12 +9,3 @@ class ArticleAdmin(admin.ModelAdmin):
 	list_filter = ['created_date']
 
 admin.site.register(Article, ArticleAdmin)
-
-
-
-	# title = models.CharField(max_length=250)
-	# url = models.CharField(max_length=250, unique=True)
-	# body = models.TextField()
-	# article_summary = models.TextField(default="")
-	# list_of_keywords = models.TextField(default="")
-	# created_date = models.DateTimeField(default=timezone.now)
