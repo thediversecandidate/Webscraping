3944f2ecd171ff2e7bd0a7135e3fc939250ce080
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-06-11 20:20:53 +0000

    Added article_summary and list_of_keywords Field to Model : Article

--- derrick/api/models.py
+++ derrick/api/models.py
@@ -7,6 +7,8 @@ class Article(models.Model):
 	title = models.CharField(max_length=250)
 	url = models.CharField(max_length=250, unique=True)
 	body = models.TextField()
+	article_summary = models.TextField(default="")
+	list_of_keywords = models.TextField(default="")
 	created_date = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
