026d67119ecb95c5a93a397582a8ad4e36cde4df
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2021-04-27 11:02:37 +0000

    feat: added Search by 'url' in article admin page

--- django/derrick/api/admin.py
+++ django/derrick/api/admin.py
@@ -7,5 +7,6 @@ from api.models import Article
 class ArticleAdmin(admin.ModelAdmin):
 	list_display = ('title', 'url', 'created_date', 'published_date')
 	list_filter = ['created_date']
+	search_fields = ('title', 'url')
 
 admin.site.register(Article, ArticleAdmin)
